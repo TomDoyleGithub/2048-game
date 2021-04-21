a45c689f001130d2eda0c7d25a7e408fcd4646f8
Punkinut <heypunkinut@gmail.com>
2021-04-21 12:58:48 +1000

    Adds two random block when you start the game and added transitions to new game button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
-// When I click new game, it will generate two blocks in a random location
+$(".new-game").on("click", () => {
+    addBlock()
+})
+
+// Function that adds blocks to page
+let addBlock = () => {
+    let i = Math.floor(Math.random() * 16)
+    let j = Math.floor(Math.random() * 16)
+    if(i === j) {
+        j++
+    } else {
+        $(".grid-block").text(" ").css("background-color", "var(--ray-brown)")
+        let gridBlockOne = $(`#block${i}`);
+        let gridBlockTwo = $(`#block${j}`);
+        gridBlockOne.text("2").css({"background-color": "var(--two)", "color": "var(--dark-brown)"})
+        gridBlockTwo.text("2").css({"background-color": "var(--two)", "color": "var(--dark-brown)"})
+    }
+}
+
+const init = () => {
+    console.log("Initiation")
+}
+init ();
+
