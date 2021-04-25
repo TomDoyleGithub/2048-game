9f2a43a53f611252923ef27f445d5297da00951d
Punkinut <heypunkinut@gmail.com>
2021-04-25 12:40:44 +1000

    Finished all 4 block combinations

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,6 +89,24 @@ const slideBlockRightRowOne = (block0, block1, block2, block3, html0, html1, htm
         $("#grid2").html(" ")
         $("#grid1").append(html0)
         $("#grid2").append(`<section class="num${parseInt(block1) + parseInt(block2)}"><p>${parseInt(block1) + parseInt(block2)}</p></section>`);
+    } else if (allFours && block0 === block1) {
+        $("#grid0").html(" ")
+        $("#grid1").html(" ")
+        $("#grid1").append(`<section class="num${parseInt(block0) + parseInt(block1)}"><p>${parseInt(block0) + parseInt(block1)}</p></section>`);
+    } else if (allFours && block2 === block3) {
+        $("#grid0").html(" ")
+        $("#grid1").html(" ")
+        $("#grid2").html(" ")
+        $("#grid3").html(" ")
+        $("#grid3").append(`<section class="num${parseInt(block2) + parseInt(block3)}"><p>${parseInt(block2) + parseInt(block3)}</p></section>`);
+        $("#grid2").append(html1)
+        $("#grid1").append(html0)
+    } else if (allFours && block1 === block2) {
+        $("#grid0").html(" ")
+        $("#grid1").html(" ")
+        $("#grid2").html(" ")
+        $("#grid2").append(`<section class="num${parseInt(block1) + parseInt(block2)}"><p>${parseInt(block1) + parseInt(block2)}</p></section>`);
+        $("#grid1").append(html0)
     }
     // Three Matches
     if(block0 !== "" && block1 !== "" && block2 !== "" && block3 === "" && block1 === block2) {
