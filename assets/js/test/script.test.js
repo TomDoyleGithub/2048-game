8b03c3b70f9d4e7478ba0303e3eb89ad8ea6097c
Punkinut <heypunkinut@gmail.com>
2021-05-03 10:22:41 +1000

    Deleted test script

--- assets/js/test/script.test.js
+++ assets/js/test/script.test.js
@@ -1,9 +1,1 @@
 const script = require('./assets/js/script.js');
-
-describe("script", () => {
-    describe("Adding block on start", () => {
-        it("Should return two random html elements when activated"), () => {
-            
-        }
-    })
-})
